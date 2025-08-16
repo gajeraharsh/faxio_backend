3b9db2a69c7c7ed2b8ae44160376ff279990b82c
gajeraharsh514 <gajeraharsh514@gmail.com>
2025-08-16 15:34:59 +0530

    solve issue

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,7 +6,8 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
+      storeCors: "http://localhost:3000",
+      // storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
