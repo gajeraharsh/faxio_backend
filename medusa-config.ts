a8d94923a4884cc815784c8f3a77e8e565201657
gajeraharsh514 <gajeraharsh514@gmail.com>
2025-09-08 22:58:29 +0530

    solve

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,13 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions:{
+      connection:{
+        ssl:{
+          rejectUnauthorized:false,
+        }
+      }
+    },
     http: {
       // Ensure CORS for all surfaces during local dev
       storeCors: process.env.STORE_CORS || "http://localhost:3000",
